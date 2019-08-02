adb1f9bbfcc97e5ef5c0c2b5383b9c4e03b52c35
Shahzeb Qureshi <shahzeb_iam@hotmail.co.in>
2019-08-02 17:23:23 +0530

    Added setup.py for CI build

---
+++ setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+
+with open(path.join(this_directory, 'README.md')) as f:
+    long_description = f.read()
+
+setup(name='django-accountkit',
+      version='0.2.1',
+      description='Facebook accountkit support for Django',
+	  long_description=long_description,
+	  long_description_content_type='text/markdown',
+      author='Shahzeb Qureshi',
+      author_email='shahzeb_iam@outlook.com',
+	  url='https://github.com/antiproblemist/django-accountkit',
+      packages=['accountkitlogin', 'accountkitlogin.templatetags'],
+	  include_package_data = True,
+      package_data={
+		'': ['LICENSE', 'README.md'],
+      },
+	  license='MIT',
+	  classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Console',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'Intended Audience :: System Administrators',
+		  'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 2.7',
+		  'Programming Language :: Python :: 3',
+		'Programming Language :: Python :: 3.4',
+		'Programming Language :: Python :: 3.5',
+		'Programming Language :: Python :: 3.6',
+          'Topic :: Software Development',
+          ],
+      install_requires=[
+		"Django >= 1.11",
+		"requests >= 2.18.4",
+		],
+     )
