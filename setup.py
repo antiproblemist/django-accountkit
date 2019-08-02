c6a091053deb1866c1a483b4403f2e5b6f01ff52
Shahzeb Qureshi <shahzeb_iam@hotmail.co.in>
2019-08-02 17:43:39 +0530

    Updated version to 0.2.3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(this_directory, 'README.md')) as f:
     long_description = f.read()
 
 setup(name='django-accountkit',
-      version='0.2.2',
+      version='0.2.3',
       description='Facebook accountkit support for Django',
 	  long_description=long_description,
 	  long_description_content_type='text/markdown',
